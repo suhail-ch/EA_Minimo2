a40c07be354b762b30476b6321af6ce90df566e4
suhail-ch <71981346+suhail-ch@users.noreply.github.com>
2021-06-02 09:47:08 +0200

    Version3

---
+++ Backend/src/models/configuracion.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document} from 'mongoose';
+
+const ConfiguracionSchema = new Schema({
+    notificaciones: {
+        type: Boolean
+    },
+    privacidad: {
+        type: Boolean
+    },
+    seguridad: {
+        type: Boolean
+    }
+});
+
+export interface IConf extends Document {
+    notificaciones: boolean;
+    privacidad: boolean;
+    seguridad: boolean;
+}
+
+export default mongoose.model<IConf>('conf', ConfiguracionSchema);
